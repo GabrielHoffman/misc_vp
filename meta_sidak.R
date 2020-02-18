6a80cb59794ecdc542514a7f7a8549c5a57cad07
GabrielHoffman <gabrielhoffman@typhon.1425mad.mountsinai.org>
2020-02-18 11:03:01 -0500

    Initial commit

--- meta_sidak.R
+++ meta_sidak.R
@@ -6,17 +6,24 @@
 #' p-value correcting for the number of tests.
 #'
 #' @param pValues array of p-values 
+#' @param na.rm: a logical indicating whether missing values should be removed.
 #'
 #' @details
 #' The Sidak method corrects a p-value based on the number of tests performed.  
 #' Given a p-value p and n tests, the Sidak corrected p-value is 1-(1-min(p))^n 
 #'
-meta_sidak = function(pValues){
+#' @examples
+#' p = runif(10)
+#'
+#' meta_sidak( p )
+#'
+meta_sidak = function(pValues, na.rm=FALSE){
 
-	n = length(pValues)
-	p.corrected = 1 - (1 - min(pValues))^n 
-	p.corrected
+	# get the number of tests
+	# if na.rm==TRUE, get the number of tests without NA values
+	n = ifelse( na.rm, sum(!is.na(pValues)), length(pValues))
+
+	# return corrected p-value
+	1 - (1 - min(pValues, na.rm=na.rm))^n 
 }
-
-
 
